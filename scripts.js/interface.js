6f7f583cdf0683b25a3ec0764f6a75123e5a6da3
João Victor <joaovgbv@gmail.com>
2022-12-23 09:57:08 -0300

    novo commit

--- scripts.js/interface.js
+++ scripts.js/interface.js
@@ -1,27 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
- let squares = document.querySelectorAll('.square')
- squares.forEach((square) => {
-  square.addEventListener('click', handleClick)
- })
+  let squares = document.querySelectorAll('.square')
+  squares.forEach((square) => {
+    square.addEventListener('click', handleClick)
+  })
 })
 
 function handleClick(event) {
- let square = event.target
- let position = square.id
- handleMove(position)
- updateSquares()
+  let square = event.target
+  let position = square.id
+  if (handleMove(position)) {
+    setTimeout(() => {
+      (playerTime == 0) ? alert(`O jogador 1 ganhou`) : alert(`O jogador 2 ganhou`)
+
+    }, 10
+    )
+  }
+  updateSquares()
 }
 
 function updateSquares() {
 
   let squares = document.querySelectorAll('.square')
   squares.forEach((square) => {
-   let position = square.id
-   let symbol = board[position]
+    let position = square.id
+    let symbol = board[position]
 
-   if (symbol != "") { square.innerHTML = `<div class='${symbol}'></div>` 
-   }
+    if (symbol != "") {
+      square.innerHTML = `<div class='${symbol}'></div>`
+    }
   })
- 
-
 }
